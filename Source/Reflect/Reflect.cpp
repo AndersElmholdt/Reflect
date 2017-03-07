d3b2edd4fcd572d4e1637ee604d645e56e963348
Anders Elmholdt <anders@elmholdt.dk>
2017-03-07 19:43:35 +0100

    Initial commit

---
+++ Source/Reflect/Reflect.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Reflect.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Reflect, "Reflect" );
---
+++ Source/Reflect/ReflectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "ReflectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REFLECT_API AReflectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
